a7198a9a78df418d085d2c444f8bb2976ba9becf
racha <rachaelw2013@gmail.com>
2020-09-08 17:22:32 -0500

    module 2 classwork

---
+++ intro_to_functions/first_program_with_function.py
@@ -0,0 +1,13 @@
+"""Small program demonstrating Sample function main"""
+
+
+def hello():
+    print('Hello, World!')
+
+
+def main():
+    hello()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test_functions.py
@@ -0,0 +1,12 @@
+import unittest
+from main import camper_age_input
+
+
+class FunctionTestCase(unittest.TestCase):
+    """Tests operation with user input"""
+    def test_something(self):
+        self.assertEqual(False, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
