b0a936f4fbe2b7dc2c46a38f32b160c49e8dc8de
racha <rachaelw2013@gmail.com>
2020-09-12 19:52:34 -0500

    files updated to actually work

--- module3/main/list_methods.py
+++ module3/main/list_methods.py
@@ -12,44 +12,24 @@ listTwo = ['Ralph', 'Lizzy']
 print(listOne)
 
 
-def set_1():
-    print(listOne.append('Ralph'))
-    print(listOne.clear())
-
-
-def set_2():
-    print(listOne.copy())
-    print(listOne.count('Gift'))
-
-
-def set_3():
-    print(listOne.extend(listTwo))
-    print(listOne.index('Zorro'))
-    print(listOne.index('Percy'))
-
-
-def set_4():
-    print(listOne.insert(3, 'Tinker'))
-    print(listOne.remove('Tinker'))
-
-
-def set_5():
-    print(listOne.reverse())
-    print(listOne.sort())
-
-
 def main():
     index = input('Which set to run? (1-5)')
-    if index == 1:
-        set_1()
-    if index == 2:
-        set_2()
-    if index == 3:
-        set_3()
-    if index == 4:
-        set_4()
-    if index == 5:
-        set_5()
+    if index == '1':
+        print(listOne.append('Ralph'))
+        print(listOne.clear())
+    if index == '2':
+        print(listOne.copy())
+        print(listOne.count('Gift'))
+    if index == '3':
+        print(listOne.extend(listTwo))
+        print(listOne.index('Zorro'))
+        print(listOne.index('Percy'))
+    if index == '4':
+        print(listOne.insert(3, 'Tinker'))
+        print(listOne.remove('Tinker'))
+    if index == '5':
+        print(listOne.reverse())
+        print(listOne.sort())
 
 
 if __name__ == '__main__':
--- module3/main/string_methods.py
+++ module3/main/string_methods.py
@@ -10,43 +10,23 @@ the methods in python associated with strings.
 movie_role = 'Westley from The Princess Bride'
 
 
-def set_1():
-    print('capitalize: ' + movie_role.capitalize())
-    print('find: ' + str(movie_role.find('ss')))
-
-
-def set_2():
-    print('index: ' + str(movie_role.index('ley')))
-    print('isalnum: ' + str(movie_role.isalnum()))
-
-
-def set_3():
-    print('isalpha: ' + str(movie_role.isalpha()))
-    print('isdigit: ' + str(movie_role.isdigit()))
-
-
-def set_4():
-    print('islower: ' + str(movie_role.islower()))
-    print('isupper: ' + str(movie_role.isupper()))
-
-
-def set_5():
-    print('isspace: ' + str(movie_role.isspace()))
-    print('startswith: ' + str(movie_role.startswith('not')))
-
-
 def main():
     index = input('Which set to run? (1-5)')
-    if index == 1:
-        set_1()
-    if index == 2:
-        set_2()
-    if index == 3:
-        set_3()
-    if index == 4:
-        set_4()
-    if index == 5:
-        set_5()
+    if index == '1':
+        print('capitalize: ' + movie_role.capitalize())
+        print('find: ' + str(movie_role.find('ss')))
+    if index == '2':
+        print('index: ' + str(movie_role.index('ley')))
+        print('isalnum: ' + str(movie_role.isalnum()))
+    if index == '3':
+        print('isalpha: ' + str(movie_role.isalpha()))
+        print('isdigit: ' + str(movie_role.isdigit()))
+    if index == '4':
+        print('islower: ' + str(movie_role.islower()))
+        print('isupper: ' + str(movie_role.isupper()))
+    if index == '5':
+        print('isspace: ' + str(movie_role.isspace()))
+        print('startswith: ' + str(movie_role.startswith('not')))
 
 
 if __name__ == '__main__':
