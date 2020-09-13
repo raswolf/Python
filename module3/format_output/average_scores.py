4472a480737c6874b9223c1fece3eac5ce9d3d35
racha <rachaelw2013@gmail.com>
2020-09-13 15:16:34 -0500

    main() written and tested

--- module3/format_output/average_scores.py
+++ module3/format_output/average_scores.py
@@ -3,7 +3,9 @@ Program: average_scores.py
 Author: Rachael Wolf
 Last date modified: 09/12/2020
 
-The purpose of this program is to
+The purpose of this program is to get a student's first and
+last names, their age, and three scores as input, then output
+the mame, age, and average score.
 """
 
 
@@ -11,4 +13,26 @@ def average():
     score1 = input('first score: ')
     score2 = input('second score: ')
     score3 = input('third score: ')
-    return (int(score1) + int(score2) + int(score3))/3
+    return (float(score1) + float(score2) + float(score3))/3.0
+
+
+def main():
+    first_name = input('Student\'s first name: ')
+    last_name = input('Student\'s last name: ')
+    age = input('Student\'s age: ')
+    average_scores = average()
+    print(last_name + ', ' + first_name + ': age ' + age, end=',')
+    print(' average grade: {0:.2f}'.format(average_scores))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Student's first name: Jay
+# Student's last name: Luck
+# Student's age: 14
+# first score: 95
+# second score: 81
+# third score: 77
+# Luck, Jay: age 14, average grade: 84.33
--- module3/test/test_average_scores.py
+++ module3/test/test_average_scores.py
@@ -13,8 +13,12 @@ from module3.format_output.average_scores import average
 
 class FunctionTestCase(unittest.TestCase):
     """Tests operation with user input 70, 82, 67"""
-    def test_average(self):
+    def test1_average(self):
         self.assertEqual(average(), 73)
+
+    """Tests operation with user input 95, 81, 77"""
+    def test2_average(self):
+        self.assertEqual(average(), 84.33)
 
 
 if __name__ == '__main__':
