b57a0a05e1ad306826cc37d31f91877058ec60a5
racha <rachaelw2013@gmail.com>
2020-09-12 22:08:34 -0500

    Input/Output assignment files

---
+++ module3/format_output/average_scores.py
@@ -0,0 +1,11 @@
+"""
+Program: average_scores.py
+Author: Rachael Wolf
+Last date modified: 09/12/2020
+
+The purpose of this program is to
+"""
+
+
+def average():
+    pass
---
+++ module3/test/test_average_scores.py
@@ -0,0 +1,21 @@
+"""
+Program: test_average_scores.py
+Author: Rachael Wolf
+Last date modified: 09/12/2020
+
+The purpose of this program is to contain
+unit tests for average_scores.py.
+"""
+
+import unittest
+from module3.format_output.average_scores import average
+
+
+class FunctionTestCase(unittest.TestCase):
+    """Tests operation with user input _____"""
+    def test_average(self):
+        self.assertEqual(average(), 60)
+
+
+if __name__ == '__main__':
+    unittest.main()
