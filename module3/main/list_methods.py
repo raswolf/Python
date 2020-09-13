0a21faff047edfde0704869a5b18dc33f7a6202f
racha <rachaelw2013@gmail.com>
2020-09-12 19:40:36 -0500

    files updated to be easier to use

--- module3/main/list_methods.py
+++ module3/main/list_methods.py
@@ -8,26 +8,49 @@ the methods in python associated with lists.
 """
 
 listOne = ['Rose', 'Zorro', 'Gift']
+listTwo = ['Ralph', 'Lizzy']
 print(listOne)
 
-# part 1
-print(listOne.append('Ralph'))
-print(listOne.clear())
 
-# part 2
-print(listOne.copy())
-print(listOne.count('Gift'))
+def set_1():
+    print(listOne.append('Ralph'))
+    print(listOne.clear())
 
-# part 3
-listTwo = ['Ralph', 'Lizzy']
-print(listOne.extend(listTwo))
-print(listOne.index('Zorro'))
-print(listOne.index('Percy'))
 
-# part 4
-print(listOne.insert(3, 'Tinker'))
-print(listOne.remove('Tinker'))
+def set_2():
+    print(listOne.copy())
+    print(listOne.count('Gift'))
 
-# part 5
-print(listOne.reverse())
-print(listOne.sort())
+
+def set_3():
+    print(listOne.extend(listTwo))
+    print(listOne.index('Zorro'))
+    print(listOne.index('Percy'))
+
+
+def set_4():
+    print(listOne.insert(3, 'Tinker'))
+    print(listOne.remove('Tinker'))
+
+
+def set_5():
+    print(listOne.reverse())
+    print(listOne.sort())
+
+
+def main():
+    index = input('Which set to run? (1-5)')
+    if index == 1:
+        set_1()
+    if index == 2:
+        set_2()
+    if index == 3:
+        set_3()
+    if index == 4:
+        set_4()
+    if index == 5:
+        set_5()
+
+
+if __name__ == '__main__':
+    main()
--- module3/main/string_methods.py
+++ module3/main/string_methods.py
@@ -7,24 +7,47 @@ The purpose of this program is to explore
 the methods in python associated with strings.
 """
 
-movie_role = 'someone in something (decisions are hard)'
+movie_role = 'Westley from The Princess Bride'
 
-# part 1
-print(movie_role.capitalize())
-print(movie_role.find('eth'))
 
-# part 2
-print(movie_role.index('one'))
-print(movie_role.isalnum())
+def set_1():
+    print('capitalize: ' + movie_role.capitalize())
+    print('find: ' + str(movie_role.find('ss')))
 
-# part 3
-print(movie_role.isalpha())
-print(movie_role.isdigit())
 
-# part 4
-print(movie_role.islower())
-print(movie_role.isupper())
+def set_2():
+    print('index: ' + str(movie_role.index('ley')))
+    print('isalnum: ' + str(movie_role.isalnum()))
 
-# part 5
-print(movie_role.isspace())
-print(movie_role.startswith('not'))
+
+def set_3():
+    print('isalpha: ' + str(movie_role.isalpha()))
+    print('isdigit: ' + str(movie_role.isdigit()))
+
+
+def set_4():
+    print('islower: ' + str(movie_role.islower()))
+    print('isupper: ' + str(movie_role.isupper()))
+
+
+def set_5():
+    print('isspace: ' + str(movie_role.isspace()))
+    print('startswith: ' + str(movie_role.startswith('not')))
+
+
+def main():
+    index = input('Which set to run? (1-5)')
+    if index == 1:
+        set_1()
+    if index == 2:
+        set_2()
+    if index == 3:
+        set_3()
+    if index == 4:
+        set_4()
+    if index == 5:
+        set_5()
+
+
+if __name__ == '__main__':
+    main()
