867adcdd4b2c20007aa1ea707777d4386333ae8f
racha <rachaelw2013@gmail.com>
2020-09-21 21:34:10 -0500

    10 <= price < 30 case tests created

--- module4/test/test_coupon_calculations.py
+++ module4/test/test_coupon_calculations.py
@@ -37,6 +37,31 @@ class FunctionTestCase(unittest.TestCase):
     def test6_set1(self):
         self.assertAlmostEqual(calculate_order(10, 10, 20), 6.55, places=4)
 
+    """Tests operation with user input price = 20, cash_coupon = 5, percent_coupon = 10
+    Shipping will be $7.95, tax is 6%"""
+    def test1_set2(self):
+        self.assertAlmostEqual(calculate_order(20, 5, 10), 22.65, places=4)
+
+    """Tests operation with user input 20, 5, 15"""
+    def test2_set2(self):
+        self.assertAlmostEqual(calculate_order(20, 5, 15), 21.90, places=4)
+
+    """Tests operation with user input 20, 5, 20 """
+    def test3_set2(self):
+        self.assertAlmostEqual(calculate_order(20, 5, 20), 21.15, places=4)
+
+    """Tests operation with user input 20, 10, 10 """
+    def test4_set2(self):
+        self.assertAlmostEqual(calculate_order(20, 10, 10), 18.15, places=4)
+
+    """Tests operation with user input 20, 10, 15 """
+    def test5_set2(self):
+        self.assertAlmostEqual(calculate_order(20, 10, 15), 17.65, places=4)
+
+    """Tests operation with user input 20, 10, 20 """
+    def test6_set2(self):
+        self.assertAlmostEqual(calculate_order(20, 10, 20), 17.15, places=4)
+
 
 if __name__ == '__main__':
     unittest.main()
