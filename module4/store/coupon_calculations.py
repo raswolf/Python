15a8983fb01d10bd0df7378672e7664a479cfef3
racha <rachaelw2013@gmail.com>
2020-09-21 21:56:47 -0500

    30 <= price < 50 case tested and confirmed

--- module4/store/coupon_calculations.py
+++ module4/store/coupon_calculations.py
@@ -17,10 +17,9 @@ def calculate_order(price, cash_coupon, percent_coupon):
         shipping = 5.95
     elif 10 <= price < 30:
         shipping = 7.95
-    if cash_coupon == 5:
-        payment -= 5
-    elif cash_coupon == 10:
-        payment -= 10
+    elif 30 <= price < 50:
+        shipping = 11.95
+    payment -= cash_coupon
     payment -= payment * (percent_coupon/100)
     return max(payment, 0) + tax + shipping
 
