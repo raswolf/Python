26112ceefd1c28104b9b94bd5baf90c61646ecca
racha <rachaelw2013@gmail.com>
2020-09-21 22:24:57 -0500

    assertion tested for position 1

---
+++ module4/input_validation/validation_with_try.py
@@ -0,0 +1,28 @@
+"""
+Program: validation_with_try.py
+Author: Rachael Wolf
+Last date modified: 09/21/2020
+
+The purpose of this program is to get a student's first and
+last names, their age, and three scores as input, then output
+the mame, age, and average score.
+"""
+
+
+def average(score1, score2, score3):
+    if score1 < 0:
+        raise ValueError
+    return (float(score1) + float(score2) + float(score3))/3.0
+
+
+def main():
+    first_name = input('Student\'s first name: ')
+    last_name = input('Student\'s last name: ')
+    age = input('Student\'s age: ')
+    average_scores = average()
+    print(last_name + ', ' + first_name + ': age ' + age, end=',')
+    print(' average grade: {0:.2f}'.format(average_scores))
+
+
+if __name__ == '__main__':
+    main()
--- module4/test/test_validation_with_try.py
+++ module4/test/test_validation_with_try.py
@@ -7,11 +7,11 @@ The purpose of this program is to contain
 unit tests for average_scores.py.
 """
 import unittest
-from module3.format_output.average_scores import average
+from module4.input_validation.validation_with_try import average
 
 
 class FunctionTestCase(unittest.TestCase):
-    def test_average_exception(self):
+    def test_average_exception_first_score(self):
         with self.assertRaises(ValueError):
             average(-90, 89, 78)
 
