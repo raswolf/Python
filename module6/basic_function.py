74e2de2e1da7201643b42f2c243c9ea7c6264f90
racha <rachaelw2013@gmail.com>
2020-10-03 13:46:23 -0500

    basic_function.py fixed and completed

--- module6/basic_function.py
+++ module6/basic_function.py
@@ -8,6 +8,7 @@ The purpose of this program is to explore functions and their creation.
 
 
 def is_convertible_to_float(value):
+    """Tests whether a value is convertible to a float"""
     try:
         float(value)
         return True
@@ -24,7 +25,7 @@ def hourly_employee_input():
     hours = input('please enter your hours worked (round to the nearest hour): ')
     while not hours_check:
         if not hours.isdigit():
-            print('that is not a valid input. hours worked must be an integer')
+            print('that is not a valid input. hours worked must be a positive integer')
             hours = input('please enter your hours worked (round to the nearest hour): ')
         elif int(hours) <= 0:
             print('that is not a valid input. hours worked must be positive')
@@ -35,8 +36,18 @@ def hourly_employee_input():
     while not rate_check:
         if not is_convertible_to_float(rate):
             print('that is not a valid input. pay rate must be a floating-point number')
-            hours = input('please enter your hourly pay rate as a decimal: ')
+            rate = input('please enter your hourly pay rate as a decimal: ')
+        elif float(rate) <= 0:
+            print('that is not a valid input. pay rate must be positive')
+            rate = input('please enter your hourly pay rate as a decimal: ')
         else:
             rate_check = True
     print(name + ' worked ' + hours + ' hours at ' + rate + '$ per hour')
-    pass
+
+
+def main():
+    hourly_employee_input()
+
+
+if __name__ == '__main__':
+    main()
