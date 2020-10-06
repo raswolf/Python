d9c6d4d48afda3d16fd6bc6ece4f1b6271d06cd6
racha <rachaelw2013@gmail.com>
2020-10-05 20:44:42 -0500

    validation and defaults file and tests created

---
+++ module6/more_functions/validate_input_in_functions.py
@@ -0,0 +1,18 @@
+"""
+Program: validate_input_in_functions.py
+Author: Rachael Wolf
+Last date modified: 10/05/2020
+
+The purpose of this program is to explore default values in function structuring.
+"""
+
+
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """Takes a test_name, test_score, and invalid_message.  Validates the test_score,
+    asks the user for a valid test score until it is in the range, then prints valid input as 'Test name: ##'.
+    :param test_name mandatory.  The name of the test taker
+    :param test_score optional, with default value of 0 (zero). should be between 0-100.
+    :param invalid_message optional, with default value 'Invalid test score, try again!'
+    :returns a string formatted as 'Test name: ##'"""
+    # return { test_name: test_score} before the return statement for later use.
+    pass
---
+++ module6/test_functions/test_validate_input_in_functions.py
@@ -0,0 +1,44 @@
+"""
+Program: test_validate_input_in_functions.py
+Author: Rachael Wolf
+Last date modified: 10/05/2020
+
+The purpose of this program is to contain
+unit tests for validate_input_in_functions.py.
+"""
+
+import unittest
+from module6.more_functions.validate_input_in_functions import score_input
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_score_input_test_name(self):
+        """checking that the function works with only the mandatory argument"""
+        self.assertEqual(score_input('Maggie'), 'Maggie: 00')
+
+    def test_score_input_test_score_valid(self):
+        """checking that the function works with the mandatory argument and the score option:
+        one good input"""
+        self.assertEqual(score_input('Maggie', 56), 'Maggie: 56')
+
+    def test_score_input_test_score_below_range(self):
+        """checking that the function works with the mandatory argument and the score option"""
+        self.assertEqual(score_input('Maggie', -56), 'Invalid test score, try again!')
+
+    def test_score_input_test_score_above_range(self):
+        """checking that the function works with the mandatory argument and the score option:"""
+        self.assertEqual(score_input('Maggie', 156), 'Invalid test score, try again!')
+
+    def test_score_input_test_score_non_numeric(self):
+        """checking that the function does not work with a non-numeric score option"""
+        with self.assertRaises(ValueError):
+            score_input('Maggie', 'what')
+
+    def test_score_input_invalid_message(self):
+        """checking that the function works with the mandatory argument and the score option:"""
+        self.assertEqual(score_input('Maggie', 56, 'invalid'), 'Maggie: 56')
+
+
+if __name__ == '__main__':
+    unittest.main()
