be8062b1dcfe4070f46ae38a87a479489faa6a23
racha <rachaelw2013@gmail.com>
2020-10-12 18:47:01 -0500

    tests updated

--- module7/test_collections/test_sort_and_search_list.py
+++ module7/test_collections/test_sort_and_search_list.py
@@ -8,7 +8,7 @@ unit tests for sort_and_search_list.py.
 """
 
 import unittest
-from module7.fun_with_collections.sort_and_search_list import search_list
+from module7.fun_with_collections.sort_and_search_list import search_list, sort_list
 
 
 class MyTestCase(unittest.TestCase):
@@ -18,6 +18,12 @@ class MyTestCase(unittest.TestCase):
 
     def test_search_list_not_found(self):
         self.assertEqual(search_list(5.5, [2.1, 3.4, 8.2]), -1)
+
+    def test_sort_list1(self):
+        self.assertEqual(sort_list([2.1, 3.4, 8.2]), [2.1, 3.4, 8.2])
+
+    def test_sort_list2(self):
+        self.assertEqual(sort_list([7.0, 2.2, 6.3, 5.5]), [2.2, 5.5, 6.3, 7.0])
 
 
 if __name__ == '__main__':
