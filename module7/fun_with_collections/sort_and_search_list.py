baf7e9c7fb3311f5f8874a51f1d545c9581e866b
racha <rachaelw2013@gmail.com>
2020-10-12 16:53:02 -0500

    functions updated, tests passed

--- module7/fun_with_collections/sort_and_search_list.py
+++ module7/fun_with_collections/sort_and_search_list.py
@@ -11,10 +11,13 @@ import module7.fun_with_collections.basic_list
 
 def sort_list(a_list):
     """sorts a list.  Does not return, because it alters the list used as input"""
-    pass
+    a_list.sort()
 
 
 def search_list(item, a_list):
     """searches a list for a specified item and returns the index of that item
     :returns the index of the search item, or -1 if the item is not in the list"""
-    pass
+    for n in a_list:
+        if n == item:
+            return a_list.index(n)
+    return -1
