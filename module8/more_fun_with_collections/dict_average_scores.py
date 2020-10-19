23438aaa6e5e7c2fc7e85706b46f24689831aa7c
racha <rachaelw2013@gmail.com>
2020-10-19 15:24:09 -0500

    functions created

---
+++ module8/more_fun_with_collections/dict_average_scores.py
@@ -0,0 +1,51 @@
+"""
+Program: dict_average_scores.py
+Author: Rachael Wolf
+Last date modified: 10/19/2020
+
+The purpose of this program is to get a student's first and
+last names, their age, and three scores as input, then output
+the mame, age, and average score. This is an updated version
+of average_scores.py
+"""
+
+
+def get_test_scores():
+    """Creates an empty dictionary
+    Prompts the user to input the number of test scores, stores in num_scores
+    uses a loop to get each of the num_scores tests
+    Prompts the user to input a test score, stores in score. (Validate the input!)
+    Adds score to scores_dict, updating the dictionary"""
+    scores_dict = dict()
+    num_scores = input("How many scores? ")
+    while not num_scores.isdigit() or int(num_scores) < 1:
+        print("There must be at least one score")
+        num_scores = input("How many scores? ")
+    for n in range(1, int(num_scores) + 1):
+        score = input("Enter score #" + str(n) + ": ")
+        if not score.isdigit() or int(score) < 0:
+            raise ValueError
+        else:
+            scores_dict.update({str(n): int(score)})
+    return scores_dict
+
+
+def average_scores(score_dict):
+    """accepts a dictionary of scores and prints the average score report as
+     a formatted string"""
+    pass
+    score_sum = 0.0
+    print('Result:', end=' ')
+    for n in range(1, len(score_dict) + 1):
+        score_sum += score_dict[str(n)]
+    print('current average {0:.2f}'.format(score_sum/len(score_dict)))
+    # return
+
+
+def main():
+    average_scores(get_test_scores())
+    # 4 scores: 78, 56, 89, 72 -----------> output: Result: current average 73.75
+
+
+if __name__ == '__main__':
+    main()
