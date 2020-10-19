dc1bef5d9415b121b3880d7439806de1d84cfd88
racha <rachaelw2013@gmail.com>
2020-10-19 13:10:30 -0500

    function written and tested

--- module8/more_fun_with_collections/set_membership.py
+++ module8/more_fun_with_collections/set_membership.py
@@ -10,4 +10,8 @@ The purpose of this program is to create a list of three items from user input
 def in_set(q_set, element):
     """accepts a set and return a boolean value stating if the element is in the set
     :returns True if the element is in the set, false otherwise"""
-    pass
+    for e in q_set:
+        if e == element:
+            return True
+    return False
+
