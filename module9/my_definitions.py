7020a5e43a4cc2d66353eedee2a18c6ad73287be
racha <rachaelw2013@gmail.com>
2020-10-25 14:34:18 -0500

    docstrings improved

--- module9/my_definitions.py
+++ module9/my_definitions.py
@@ -5,25 +5,29 @@ Last date modified: 10/24/2020
 
 this program is a Python module that includes the following:
 A function greeting that prints a friendly greeting
-A function message that prints you as the author of the code
+A function message that prints me as the author of the code
 A function print_dict that accepts a dictionary and prints the pairs (key, value) one per line
 A function print_set that accepts a set and prints the set one item per line
 """
 
 
 def greeting(name):
+    """prints a friendly greeting addressed to the person whose name is used as input"""
     print('Hello, ' + name)
 
 
 def message():
+    """prints a message crediting me as the author of the code"""
     print('This code was written by Rachael Wolf')
 
 
 def print_dict(dictionary_to_print):
+    """accepts a dictionary and prints the pairs (in the form 'key: value') one per line"""
     for n in dictionary_to_print:
         print(n + ': ' + str(dictionary_to_print[n]))
 
 
 def print_set(set_to_print):
+    """accepts a set and prints the set elements one item per line"""
     for n in set_to_print:
         print(n)
