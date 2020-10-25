23910494d81306a42c5541c952803604afdd08aa
racha <rachaelw2013@gmail.com>
2020-10-25 17:28:45 -0500

    imports fixed

---
+++ module9/scraper.py
@@ -0,0 +1,20 @@
+"""
+Program: scraper.py
+Author: Rachael Wolf
+Last date modified: 10/25/2020
+
+this program is
+"""
+import requests
+import bs4 as bs
+
+
+url = 'https://www.dmacc.edu/schedule/Pages/result.aspx?Term=201903&Campus=1;4&S'
+response = requests.get(url)
+html = response.content
+f = open("requestResult.txt", "w+")
+f.writelines(str(html))
+f.close()
+
+soup = bs.BeautifulSoup(open("requestResult.txt"), 'html.parser')
+print(soup.prettify())
