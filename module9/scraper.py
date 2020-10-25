e944da49f892cb4256036fefa770b71f8c2f159e
racha <rachaelw2013@gmail.com>
2020-10-25 17:38:50 -0500

    docstring improved

--- module9/scraper.py
+++ module9/scraper.py
@@ -3,7 +3,8 @@ Program: scraper.py
 Author: Rachael Wolf
 Last date modified: 10/25/2020
 
-this program is
+the purpose of this program is to get html data from a page in the dmacc calendar, store the
+html in a text document, and print an easier to read version of the text.
 """
 import requests
 import bs4 as bs
