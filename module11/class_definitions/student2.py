b41b3c1a0f53605f75222debd97c4a1e27ce8701
racha <rachaelw2013@gmail.com>
2020-11-09 17:31:45 -0600

    Student class updated to be subclass of Person

--- module11/class_definitions/student2.py
+++ module11/class_definitions/student2.py
@@ -3,10 +3,11 @@ Program: student.py
 Author: Rachael Wolf
 Last date modified: 11/8/2020
 
-This program contains a Class called Student and a function relating to it
-The functions included are: __str__(), change_major(), update_gpa() and display()
+This program contains a Class called Student and functions relating to it
+The functions overridden are: __str__() and display()
+The functions added are change_major() and update_gpa()
 """
-import module11.class_definitions.Person as p
+import module11.class_definitions.person as p
 
 
 def is_convertible_to_float(value):
@@ -18,20 +19,20 @@ def is_convertible_to_float(value):
         return False
 
 
-class Student:
+class Student(p.Person):
     """Student class"""
-    def __init__(self, person, major, gpa=0.0):
-
-        if not isinstance(person, p.Person):
-            raise ValueError
+    def __init__(self, student_id, fname, lname, major='Computer Science', gpa=0.0):
+        super().__init__(fname, lname)
         if not is_convertible_to_float(gpa):
             raise ValueError
-        self.person = person
+        if not isinstance(student_id, int):
+            raise ValueError
+        self.student_id = student_id
         self.major = major
         self.gpa = gpa
 
     def __str__(self):
-        return self.person.last_name + ", " + self.person.first_name + " has major " \
+        return self.last_name + ", " + self.first_name + " has major " \
                + self.major + " with gpa: " + str(self.gpa)
 
     def change_major(self, new_major):
@@ -44,17 +45,15 @@ class Student:
             self.gpa = new_gpa
 
     def display(self):
-        return self.person.last_name + ", " + self.person.first_name + " has major " \
-               + self.major + " with gpa: " + str(self.gpa)
+        return self.last_name + ", " + self.first_name + ":(" + str(self.student_id) + ") " \
+               + self.major + " gpa: " + str(self.gpa)
 
 
 # driver
-me = p.Person("Wolf", "Rachael")
-student_me = Student(me, "Computer Science", 4.0)
-print(student_me.display())
-student_me.change_major("Being Awesome!")
-student_me.update_gpa(3.0)
-print(student_me.display())
-del student_me
-del me
-
+my_student = Student(900111111, 'Song', 'River')
+print(my_student.display())
+my_student = Student(900111111, 'Song', 'River', 'Computer Engineering')
+print(my_student.display())
+my_student = Student(900111111, 'Song', 'River', 'Computer Engineering', 4.0)
+print(my_student.display())
+del my_student
